3e374c98134621a47648436689fafa54710937b9
LoadingByte <loadingbyte@gmail.com>
2014-02-05 21:49:04 +0100

    Integration uses the Query API
    Added: QuarterBukkitIntegration no longer uses the bukkit file feed for fetching files. It now uses some parts of the Query API from the main plugin. That splits the class into several submodules (the query api) and reduces the complexity of the class.

--- integration/src/main/java/com/quartercode/quarterbukkit/api/FileUtils.java
+++ integration/src/main/java/com/quartercode/quarterbukkit/api/FileUtils.java
@@ -1,22 +1,22 @@
 /*
- * This file is part of QuarterBukkit-Plugin.
+ * This file is part of QuarterBukkit-Integration.
  * Copyright (c) 2012 QuarterCode <http://www.quartercode.com/>
  *
- * QuarterBukkit-Plugin is free software: you can redistribute it and/or modify
+ * QuarterBukkit-Integration is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * QuarterBukkit-Plugin is distributed in the hope that it will be useful,
+ * QuarterBukkit-Integration is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with QuarterBukkit-Plugin. If not, see <http://www.gnu.org/licenses/>.
+ * along with QuarterBukkit-Integration. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.quartercode.quarterbukkit;
+package com.quartercode.quarterbukkit.api;
 
 import java.io.File;
 import java.io.FileInputStream;
